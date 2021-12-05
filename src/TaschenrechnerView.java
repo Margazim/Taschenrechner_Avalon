b4d4afe9bb8fe1b11867efea4d681521b87c10d1
simon <veser@mail.hs-ulm.de>
2021-12-05 17:21:20 +0100

    Commit

--- src/TaschenrechnerView.java
+++ src/TaschenrechnerView.java
@@ -1,24 +1,56 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TaschenrechnerView {
     private JPanel mainPanel;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
-    private JButton button9;
-    private JButton button10;
-    private JButton button11;
-    private JButton button12;
-    private JButton button13;
-    private JButton button14;
-    private JButton button15;
-    private JButton button16;
-    private JTextField textField1;
+    private JButton sevenButton;
+    private JButton eightButton;
+    private JButton nineButton;
+    private JButton fourButton;
+    private JButton fiveButton;
+    private JButton sixButton;
+    private JButton oneButton;
+    private JButton twoButton;
+    private JButton threeButton;
+    private JButton deleteButton;
+    private JButton nullButton;
+    private JButton resultButton;
+    private JButton plusButton;
+    private JButton minusButton;
+    private JButton multButton;
+    private JButton divideButton;
+    private JTextField anzeige;
+
+    public TaschenrechnerView() {
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                anzeige.setText(e.toString());
+                anzeige.getText();
+
+//                String zahlString = .getText();
+//                int zahl = Integer.parseInt(zahlString);
+//                anzeige.setText(anzeige.getText() + zahlString);
+            }
+        };
+        sevenButton.addActionListener(listener);
+        eightButton.addActionListener(listener);
+        nineButton.addActionListener(listener);
+        fourButton.addActionListener(listener);
+        fiveButton.addActionListener(listener);
+        sixButton.addActionListener(listener);
+        oneButton.addActionListener(listener);
+        twoButton.addActionListener(listener);
+        threeButton.addActionListener(listener);
+        deleteButton.addActionListener(listener);
+        nullButton.addActionListener(listener);
+        resultButton.addActionListener(listener);
+        plusButton.addActionListener(listener);
+        minusButton.addActionListener(listener);
+        multButton.addActionListener(listener);
+        divideButton.addActionListener(listener);
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here

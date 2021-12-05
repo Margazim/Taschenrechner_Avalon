81219eaf3744c121265d8abbd1b440daafd64809
Margazim <glorie98@googlemail.com>
2021-12-05 16:52:46 +0100

    asdg

--- src/TaschenrechnerModel.java
+++ src/TaschenrechnerModel.java
@@ -5,7 +5,33 @@ public class TaschenrechnerModel {
     private String operator;
 
     public int getErgebnis(){
-        return -1;
+        switch(operator){
+            case "+":
+                return ersterOperand+zweiterOperand;
+            case "-":
+                return ersterOperand-zweiterOperand;
+            case "/":
+                return ersterOperand/zweiterOperand;
+            case "*":
+                return ersterOperand*zweiterOperand;
+            default:
+                return zweiterOperand;
+        }
+
     }
+    public void zuruecksetzen(){
+        operator = null;
+    }
+    public void  setErsterOperand(int opd){
+        this.zweiterOperand = opd;
+    }
+    public void  setZweiterOperand(int opd){
+        this.ersterOperand = opd;
+    }
+    public void setOperator(String operator){
+        this.operator = operator;
+    }
+
+
 
 }
